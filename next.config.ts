457abeff6517fc5a3b80bff23a3039a5c4be9774
Dave Kok <davekok.main@gmail.com>
2026-03-06 12:21:13 +0100

    toml v6

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Dit dwingt de statische export af
+  images: {
+    unoptimized: true, // Nodig omdat een statische export geen image-server heeft
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
